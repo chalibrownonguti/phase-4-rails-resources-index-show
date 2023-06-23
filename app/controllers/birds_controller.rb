7037f2025af37d87cca6df0befedd0ef59ea83db
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-06-23 17:36:25 +0300

    update

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,5 +6,10 @@ class BirdsController < ApplicationController
      
     def show
         bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else
+            render json: {error: "Bird not found"}, status: :not_found
+        end
     end
 end
